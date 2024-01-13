bbb5d04f02094166c0a4769ffaa35a8fe9841427
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-01-13 11:21:14 +0200

    Update tag mutation

--- graphql/mutationType.js
+++ graphql/mutationType.js
@@ -3,6 +3,7 @@ const {
 } = require('graphql');
 
 const createTag = require('./mutations/tag/createTag');
+const updateTag = require('./mutations/tag/updateTag');
 const deleteTag = require('./mutations/tag/deleteTag');
 const createUser = require('./mutations/user/createUser');
 const createRecipe = require('./mutations/recipe/createRecipe');
@@ -11,6 +12,7 @@ const mutationType = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
         createTag: createTag,
+        updateTag: updateTag,
         deleteTag: deleteTag,
         createUser:createUser,
         createRecipe:createRecipe,
